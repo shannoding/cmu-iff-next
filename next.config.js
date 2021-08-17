1a3b1f59110dd3f2a24158a90470c8f8d69ea7c5
Shannon Ding <shannon_ding@yahoo.com>
2021-08-17 09:34:38 -0400

    new path for git pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-  basePath: '/out',
-  assetPrefix: '/out',
-  basePublicPath: '/out'
+  basePath: '/cmu-iff-next/out',
+  assetPrefix: '/cmu-iff-next/out/',
+  basePublicPath: '/cmu-iff-next/out/'
   // basePath: '/Users/shannon/Desktop/cmu-iff-next/out',
   // assetPrefix: '/Users/shannon/Desktop/cmu-iff-next/out',
   // basePublicPath: '/Users/shannon/Desktop/cmu-iff-next/out'
