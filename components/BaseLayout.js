bb82da1b9e444ee212936f5baf07d45b4c24182e
Shannon Ding <shannon_ding@yahoo.com>
2021-08-17 10:12:34 -0400

    jquery before interactive loads

--- components/BaseLayout.js
+++ components/BaseLayout.js
@@ -52,6 +52,7 @@ export default function BaseLayout({ children, title, activeItem }) {
         src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
         crossOrigin="anonymous" 
+        strategy="beforeInteractive"
         />
 
         <Script 
