0682efdad82e4202233a39819ab96239d999c448
Shannon Ding <shannon_ding@yahoo.com>
2021-08-17 09:25:27 -0400

    added out/ to try file:// issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-  basePath: '/Users/shannon/Desktop/cmu-iff-next/out',
-  assetPrefix: '/Users/shannon/Desktop/cmu-iff-next/out',
-  basePublicPath: '/Users/shannon/Desktop/cmu-iff-next/out'
+  basePath: '/out',
+  assetPrefix: '/out',
+  basePublicPath: '/out'
+  // basePath: '/Users/shannon/Desktop/cmu-iff-next/out',
+  // assetPrefix: '/Users/shannon/Desktop/cmu-iff-next/out',
+  // basePublicPath: '/Users/shannon/Desktop/cmu-iff-next/out'
 }
