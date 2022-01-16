0aeda4999818fc1ef92b2bd8c1758525dadcafcf
shannoding <shannon_ding@yahoo.com>
2022-01-16 12:15:03 -0500

    deploy ghp again?

---
+++ out/_next/static/Hrjx-ulOOkVdegM0RbqnQ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fteam","\u002Ftickets","\u002Fprevious_festivals","\u002Fschedule","\u002Fsponsors","\u002F[article]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
